054b2b6ca1c17f55ba9e1870346d5552563b76ff
Fightingpainter <lechimnorepe@gmail.com>
2025-02-19 17:30:13 +0100

    shop ui! yay

--- src/main/java/net/fightingpainter/mc/towntime/coined/screens/ShopMenu.java
+++ src/main/java/net/fightingpainter/mc/towntime/coined/screens/ShopMenu.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.SimpleContainerData;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 
 public class ShopMenu extends AbstractContainerMenu {
@@ -27,7 +28,17 @@ public class ShopMenu extends AbstractContainerMenu {
         this.container = container;
         this.data = data;
 
-        // Add slots and data slots here
+        //Player Inventory
+        for (int row = 0; row < 3; ++row) {
+            for (int col = 0; col < 9; ++col) {
+                this.addSlot(new Slot(playerInventory, col + row * 9 + 9, 108 + col * 18, 84 + row * 18));
+            }
+        }
+
+        //Player Hotbar
+        for (int col = 0; col < 9; ++col) {
+            this.addSlot(new Slot(playerInventory, col, 108 + col * 18, 142));
+        }
     }
 
     @Override
@@ -36,8 +47,34 @@ public class ShopMenu extends AbstractContainerMenu {
     }
 
     @Override
-    public ItemStack quickMoveStack(Player arg0, int arg1) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'quickMoveStack'");
+    public ItemStack quickMoveStack(Player player, int slotIndex) {
+        Slot slot = this.slots.get(slotIndex);
+        if (slot == null || !slot.hasItem()) {
+            return ItemStack.EMPTY;
+        }
+
+        ItemStack stack = slot.getItem();
+        ItemStack result = stack.copy();
+        
+        if (slotIndex < 27) { //if slot is in Inventory
+            //move to first slot in Hotbar
+            if (!this.moveItemStackTo(stack, 27, 36, false)) {
+                return ItemStack.EMPTY;
+            }
+        } else if (slotIndex < 36) { //if slot is in Hotbar
+            //move to first slot in Inventory
+            if (!this.moveItemStackTo(stack, 0, 27, false)) {
+                return ItemStack.EMPTY;
+            }
+        }
+
+
+        if (stack.isEmpty()) {
+            slot.set(ItemStack.EMPTY);
+        } else {
+            slot.setChanged();
+        }
+
+        return result;
     }
 }
