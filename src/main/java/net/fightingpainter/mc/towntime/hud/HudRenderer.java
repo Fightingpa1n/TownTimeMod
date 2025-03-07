e02ff581e5841b1179767d76855021aaaf8dfc8c
Fightingpainter <lechimnorepe@gmail.com>
2025-03-07 15:32:35 +0100

    Hotbar

--- src/main/java/net/fightingpainter/mc/towntime/hud/HudRenderer.java
+++ src/main/java/net/fightingpainter/mc/towntime/hud/HudRenderer.java
@@ -1,70 +1,83 @@
 package net.fightingpainter.mc.towntime.hud;
 
-import net.fightingpainter.mc.towntime.hud.bars.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.client.event.ScreenEvent;
+
+import net.fightingpainter.mc.towntime.TownTime;
+import net.fightingpainter.mc.towntime.hud.bars.*;
+
 
 public class HudRenderer {
+    private static final ResourceLocation HudBackground = ResourceLocation.fromNamespaceAndPath(TownTime.MOD_ID, "textures/hud/hud.png");
+    private static final int HudTextureWidth = 182;
+    private static final int HudTextureHeight = 60;
     
+    private static final Hotbar hotbar = new Hotbar(9, 39, 10);
+    /* 
     private static final HealthBar healthBar = new HealthBar(10, 10);
-
     private static final AbsorbtionDisplay absorbtionDisplay = new AbsorbtionDisplay(40, 10);
-    
     private static final Food foodBar = new Food(10, 30);
     private static final Thirst thirstBar = new Thirst(50, 30);
-
     private static final TemperatureDisplay temperatureDisplay = new TemperatureDisplay(10, 50);
+     */
+    // private static final XpBar xpBar = new XpBar(10, 10);
 
-    private static final Hotbar hotbar = new Hotbar(10, 70);
 
-    private static final XpBar xpBar = new XpBar(10, 10);
+
 
     public static void render(GuiGraphics graphics) {
         Minecraft minecraft = Minecraft.getInstance(); //get instance
         Player player = minecraft.player; //get player
         if (player == null) return; //if player is null, return
 
-        if(healthBar.shouldRender(player)) { //if health bar should render
-            healthBar.getParameters(player); //get health bar parameters
-            healthBar.render(graphics); //render health bar
-        }
+        int bgX = graphics.guiWidth() - HudTextureWidth;
+        int bgY = graphics.guiHeight() - HudTextureHeight;
+        graphics.blit(HudBackground, bgX, bgY, 0, 0, HudTextureWidth, HudTextureHeight, HudTextureWidth, HudTextureHeight); //render background
 
-        if(absorbtionDisplay.shouldRender(player)) { //if absorbtion display should render
-            absorbtionDisplay.getParameters(player); //get absorbtion display parameters
-            absorbtionDisplay.render(graphics); //render absorbtion display
-        }
+        hotbar.setPos(bgX, bgY);
+        hotbar.getParameters(player);
+        hotbar.render(graphics);
 
-        if(foodBar.shouldRender(player)) { //if food bar should render
-            foodBar.getParameters(player); //get food bar parameters
-            foodBar.render(graphics); //render food bar
-        }
+        // if(healthBar.shouldRender(player)) { //if health bar should render
+        //     healthBar.getParameters(player); //get health bar parameters
+        //     healthBar.render(graphics); //render health bar
+        // }
 
-        if(thirstBar.shouldRender(player)) { //if thirst bar should render
-            thirstBar.getParameters(player); //get thirst bar parameters
-            thirstBar.render(graphics); //render thirst bar
-        }
+        // if(absorbtionDisplay.shouldRender(player)) { //if absorbtion display should render
+        //     absorbtionDisplay.getParameters(player); //get absorbtion display parameters
+        //     absorbtionDisplay.render(graphics); //render absorbtion display
+        // }
 
-        if (temperatureDisplay.shouldRender(player)) { //if temperature display should render
-            temperatureDisplay.getParameters(player); //get temperature display parameters
-            temperatureDisplay.render(graphics); //render temperature display
-        }
+        // if(foodBar.shouldRender(player)) { //if food bar should render
+        //     foodBar.getParameters(player); //get food bar parameters
+        //     foodBar.render(graphics); //render food bar
+        // }
 
-        if (hotbar.shouldRender(player)) { //if hotbar should render
-            hotbar.getParameters(player); //get hotbar parameters
-            hotbar.render(graphics); //render hotbar
-        }
+        // if(thirstBar.shouldRender(player)) { //if thirst bar should render
+        //     thirstBar.getParameters(player); //get thirst bar parameters
+        //     thirstBar.render(graphics); //render thirst bar
+        // }
+
+        // if (temperatureDisplay.shouldRender(player)) { //if temperature display should render
+        //     temperatureDisplay.getParameters(player); //get temperature display parameters
+        //     temperatureDisplay.render(graphics); //render temperature display
+        // }
+
+        // if (hotbar.shouldRender(player)) { //if hotbar should render
+        //     hotbar.getParameters(player); //get hotbar parameters
+        //     hotbar.render(graphics); //render hotbar
+        // }
     }
 
 
     public static void xpBarRenderer(Screen screen, GuiGraphics graphics) {
         if (screen instanceof InventoryScreen) {
-            xpBar.getParameters(Minecraft.getInstance().player);
-            xpBar.render(graphics);
+            // xpBar.getParameters(Minecraft.getInstance().player);
+            // xpBar.render(graphics);
         }
     }
 }
--- src/main/java/net/fightingpainter/mc/towntime/hud/bars/Hotbar.java
+++ src/main/java/net/fightingpainter/mc/towntime/hud/bars/Hotbar.java
@@ -1,6 +1,5 @@
 package net.fightingpainter.mc.towntime.hud.bars;
 
-
 import net.fightingpainter.mc.towntime.TownTime;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -11,58 +10,85 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 public class Hotbar {
-    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(TownTime.MOD_ID, "textures/hud/hotbar.png"); 
-    private static final ResourceLocation SELECTED = ResourceLocation.fromNamespaceAndPath(TownTime.MOD_ID, "textures/hud/hotbar_selected.png");
-    private final int width = 154;
-    private final int height = 18;
-    private int x;
-    private int y;
+    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(TownTime.MOD_ID, "textures/hud/hotbar.png");
+    private static final ResourceLocation SELECTED_TEXTURE = ResourceLocation.fromNamespaceAndPath(TownTime.MOD_ID, "textures/hud/hotbar_selected.png");
+    private static final int width = 178;
+    private static final int height = 20;
+    
+    
+    private final int xOffset;
+    private final int yOffset;
+    private final int zIndex;
+    private int x = 0;
+    private int y = 0;
     private ItemStack[] items = new ItemStack[9];
-    private int active = 0;
+    private int selected = 0;
 
-    public Hotbar(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Hotbar(int xOffset, int yOffset, int zIndex) {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        this.zIndex = zIndex;
     }
 
     public void getParameters(Player player) { //get items
         for (int i = 0; i < 9; i++) {
             items[i] = player.getInventory().getItem(i);
         }
-        active = player.getInventory().selected;
+        selected = player.getInventory().selected;
     }
 
-
-    public boolean shouldRender(Player player) {
-        return true;
-    }
-
-
-    public void render(GuiGraphics guiGraphics) {
-        guiGraphics.blit(TEXTURE, x, y, 0, 0, width, height, width, height);
+    public void render(GuiGraphics graphics) {
         Font font = Minecraft.getInstance().font;
-        
+        int baseX = x + xOffset;
+        int baseY = y + yOffset;
+
+        graphics.pose().pushPose();
+        graphics.pose().translate(0, 0, zIndex); //set z index
+        graphics.blit(TEXTURE, baseX, baseY, 0, 0, 178, 20, 178, 20); //render hotbar
+        graphics.pose().popPose();
+
         for (int i = 0; i < 9; i++) {
-            if (i == active) {guiGraphics.blit(SELECTED, x + i * 17, y, 0, 0, 16, 16, 16, 16);}
-            if (items[i] != null) {
-                guiGraphics.renderItem(items[i], x + 1 + i * 17, y + 1);
+            int slotX = (baseX + i * 19); //slot x
+        
+            if (i == selected) { //if slot is selected render selected texture
+                graphics.pose().pushPose();
+                graphics.pose().translate(0, 0, zIndex+1); //set z index +1
+                graphics.blit(SELECTED_TEXTURE, slotX, baseY, 0, 0, 20, 20, 20, 20);
+                graphics.pose().popPose();
+            }
+
+            if (!items[i].isEmpty()) { //render items
+                graphics.pose().pushPose();
+                graphics.pose().translate(0, 0, zIndex+2); //set z index +2 (renderItem adds 150 to the z index)
+                graphics.renderItem(items[i], slotX+2, baseY+2);
+                graphics.pose().popPose();
 
                 if (items[i].getCount() > 1) {
                     String text = Integer.toString(items[i].getCount());
                     int textWidth = font.width(text);
-                    int textX = x + 17 + i * 17 - textWidth;
-                    int textY = y + 17;
-                    guiGraphics.drawString(font, text, textX, textY, 0xFFFFFF);
+                    int textHeight = font.lineHeight;
+
+                    int textX = (slotX + 19)-textWidth;
+                    int textY = (baseY + 20)-textHeight;
+                    graphics.pose().pushPose();
+                    graphics.pose().translate(0, 0, (zIndex+3)+200); //set z index +3 (+150 for above item)
+                    graphics.drawString(font, text, textX, textY, 0xFFFFFF);
+                    graphics.pose().popPose();
                 }
             }
         }
 
-        if (items[active] != null) { //if active item exists render name above hotbar
-            Component name = items[active].getHoverName();
-            int textWidth = font.width(name);
-            int textX = x + (width - textWidth) / 2;
-            int textY = y - 10;
-            guiGraphics.drawString(font, name, textX, textY, 0xFFFFFF);
-        }
+        // if (!items[active].isEmpty()) { //if active item exists render name above hotbar
+        //     Component name = items[active].getHoverName();
+        //     int textWidth = font.width(name);
+        //     int textX = baseX + (width - textWidth) / 2;
+        //     int textY = baseY - 10;
+        //     graphics.drawString(font, name, textX, textY, 0xFFFFFF);
+        // }
+    }
+
+    public void setPos(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
