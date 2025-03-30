e6393a6f90207063a562abfbb1bfa899a21f9f29
Fightingpa1n <fightingpainter@gmail.com>
2025-03-30 16:20:16 +0200

    I really need to do Temperature

--- src/main/java/net/fightingpainter/mc/towntime/food/DataHandler.java
+++ src/main/java/net/fightingpainter/mc/towntime/food/DataHandler.java
@@ -34,7 +34,14 @@ public class DataHandler {
     private static final int SLOW_HEAL_TICKS = 80; //the ticks for slow heal (default is 80, or 4 seconds)
     private static final int DAMAGE_TICKS = 80; //the ticks for damage (default is 80, or 4 seconds)
 
-    private static final float SATURATION_PER_HEAL = 6.0F; //the saturation required to heal 1 health
+    // private static final float SATURATION_PER_HEAL = 6.0F; //the saturation required to heal 1 health
+    // private static final float SLOW_HEAL_AMOUNT = 1.0F; //the amount of health to heal for slow heal
+    // private static final float SLOW_HEAL_EXHAUSTION = 6.0F; //the amount of exhaustion to add for slow heal
+
+    private static final int HYDRATION_PER_HEAL = 6; //the hydration required to heal fully
+    private static final float FAST_HEAL_AMOUNT = 1.0F; //the amount of health to heal for fast heal
+
+    private static final float THIRST_MIN_THRESHOLD = 18.0F; //the minimum thirst level for healing
     private static final float SLOW_HEAL_AMOUNT = 1.0F; //the amount of health to heal for slow heal
     private static final float SLOW_HEAL_EXHAUSTION = 6.0F; //the amount of exhaustion to add for slow heal
 
@@ -67,29 +74,50 @@ public class DataHandler {
         }
         
         //=========== Regeneration ===========\\
-        if (player.isHurt() && doRegen) { //if player is hurt and natural regeneration is enabled
-            if (food.getSaturationLevel() > 0.0F && food.getFoodLevel() >= 20 && (!thirstPreventHealing || (thirst.getHydration() > 0.0F && thirst.getThirst() >= 20))) { //if player has saturation and food level is full (same goes for thirst if enabled)
-                resetFoodTimer = false; //don't reset food timer
-                food.setTickTimer(food.getTickTimer() + 1); //increment food tick timer
-                if (food.getTickTimer() >= FAST_HEAL_TICKS) { //if food tick timer reached fast heal ticks
-                    float amount = Math.min(food.getSaturationLevel(), SATURATION_PER_HEAL); //get saturation amount available for this heal tick (max is SATURATION_PER_HEAL)
-                    player.heal(amount / SATURATION_PER_HEAL); //heal player by amount / SATURATION_PER_HEAL (1 health if amount is equal to SATURATION_PER_HEAL)
-                    food.setExhaustionLevel(food.getExhaustionLevel()+amount); //add amount to exhaustion level
+        if (player.isHurt() && doRegen) { //if pleayer is hurt and natural regeneration is enabled
+            if (thirst.getHydration() > 0.0F && thirst.getThirst() >= 20) { //if player has hydration and thirst level is full
+                resetThirstTimer = false; //don't reset thirst timer
+                thirst.setTickTimer(thirst.getTickTimer() + 1); //increment thirst tick timer
+                if (thirst.getTickTimer() >= FAST_HEAL_TICKS) { //if thirst tick timer reached fast heal ticks
+                    float amount = Math.min(thirst.getHydration(), HYDRATION_PER_HEAL); //get hydration amount available for this heal tick (max is HYDRATION_PER_HEAL)
+                    player.heal(FAST_HEAL_AMOUNT*(amount/HYDRATION_PER_HEAL)); //heal player by FAST_HEAL_AMOUNT*(amount/HYDRATION_PER_HEAL) (if amount is equal to HYDRATION_PER_HEAL, heal full FAST_HEAL_AMOUNT)
                     thirst.setExhaustion(thirst.getExhaustion()+amount); //add amount to thirst exhaustion level
-                    food.setTickTimer(0); //reset food tick timer
+                    thirst.setTickTimer(0); //reset thirst tick timer
                 }
-            } else if (food.getFoodLevel() >= 18 && (!thirstPreventHealing || thirst.getThirst() >= 18)) { //if food level is greater than or equal to 18 (same goes for thirst if enabled)
-                resetFoodTimer = false; //don't reset food timer
-                food.setTickTimer(food.getTickTimer() + 1); //increment tick timer
-                if (food.getTickTimer() >= SLOW_HEAL_TICKS) { //if food tick timer reached slow heal ticks
+            } else if (thirst.getThirst() >= THIRST_MIN_THRESHOLD) { //if thirst level is greater than or equal to THIRST_MIN_THRESHOLD
+                resetThirstTimer = false; //don't reset thirst timer
+                thirst.setTickTimer(thirst.getTickTimer() + 1); //increment thirst tick timer
+                if (thirst.getTickTimer() >= SLOW_HEAL_TICKS) { //if thirst tick timer reached slow heal ticks
                     player.heal(SLOW_HEAL_AMOUNT); //heal player by SLOW_HEAL_AMOUNT
-                    food.setExhaustionLevel(food.getExhaustionLevel()+SLOW_HEAL_EXHAUSTION); //add SLOW_HEAL_EXHAUSTION to exhaustion level
                     thirst.setExhaustion(thirst.getExhaustion()+SLOW_HEAL_EXHAUSTION); //add SLOW_HEAL_EXHAUSTION to thirst exhaustion level
-                    food.setTickTimer(0);
+                    thirst.setTickTimer(0); //reset thirst tick timer
                 }
             }
         }
 
+        // if (player.isHurt() && doRegen) { //if player is hurt and natural regeneration is enabled
+        //     if (food.getSaturationLevel() > 0.0F && food.getFoodLevel() >= 20 && (!thirstPreventHealing || (thirst.getHydration() > 0.0F && thirst.getThirst() >= 20))) { //if player has saturation and food level is full (same goes for thirst if enabled)
+        //         resetFoodTimer = false; //don't reset food timer
+        //         food.setTickTimer(food.getTickTimer() + 1); //increment food tick timer
+        //         if (food.getTickTimer() >= FAST_HEAL_TICKS) { //if food tick timer reached fast heal ticks
+        //             float amount = Math.min(food.getSaturationLevel(), SATURATION_PER_HEAL); //get saturation amount available for this heal tick (max is SATURATION_PER_HEAL)
+        //             player.heal(amount / SATURATION_PER_HEAL); //heal player by amount / SATURATION_PER_HEAL (1 health if amount is equal to SATURATION_PER_HEAL)
+        //             food.setExhaustionLevel(food.getExhaustionLevel()+amount); //add amount to exhaustion level
+        //             thirst.setExhaustion(thirst.getExhaustion()+amount); //add amount to thirst exhaustion level
+        //             food.setTickTimer(0); //reset food tick timer
+        //         }
+        //     } else if (food.getFoodLevel() >= 18 && (!thirstPreventHealing || thirst.getThirst() >= 18)) { //if food level is greater than or equal to 18 (same goes for thirst if enabled)
+        //         resetFoodTimer = false; //don't reset food timer
+        //         food.setTickTimer(food.getTickTimer() + 1); //increment tick timer
+        //         if (food.getTickTimer() >= SLOW_HEAL_TICKS) { //if food tick timer reached slow heal ticks
+        //             player.heal(SLOW_HEAL_AMOUNT); //heal player by SLOW_HEAL_AMOUNT
+        //             food.setExhaustionLevel(food.getExhaustionLevel()+SLOW_HEAL_EXHAUSTION); //add SLOW_HEAL_EXHAUSTION to exhaustion level
+        //             thirst.setExhaustion(thirst.getExhaustion()+SLOW_HEAL_EXHAUSTION); //add SLOW_HEAL_EXHAUSTION to thirst exhaustion level
+        //             food.setTickTimer(0);
+        //         }
+        //     }
+        // }
+        
         //=========== Damage ===========\\
         if (food.getFoodLevel() <= 0) { //if food level is empty
             resetFoodTimer = false; //don't reset food timer
@@ -118,84 +146,85 @@ public class DataHandler {
         if (resetThirstTimer) {thirst.setTickTimer(0);} //reset thirst tick timer
         
 
-        // ------------------- TEMPERATURE -------------------
-        if (!ModConfig.temperature.enableTemperature) return;
+        /* / ------------------- TEMPERATURE -------------------
+            if (!ModConfig.temperature.enableTemperature) return;
 
-        // Remove invalid thermoregulators
-        temperature.getNearbyThermoregulators().removeIf(pos -> player.level().getBlockEntity(pos) == null || player.distanceToSqr(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5) > 1024);
+            // Remove invalid thermoregulators
+            temperature.getNearbyThermoregulators().removeIf(pos -> player.level().getBlockEntity(pos) == null || player.distanceToSqr(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5) > 1024);
 
-        temperature.setChangeDelayTicks(Math.max(0, temperature.getChangeDelayTicks() - 1));
-        temperature.setDryTicks(temperature.getDryTicks() + 1);
+            temperature.setChangeDelayTicks(Math.max(0, temperature.getChangeDelayTicks() - 1));
+            temperature.setDryTicks(temperature.getDryTicks() + 1);
 
-        if (!player.hasEffect(TANEffects.CLIMATE_CLEMENCY)) {
-            int changeDelay = ModConfig.temperature.temperatureChangeDelay;
-            TemperatureLevel current = temperature.getTargetLevel();
-            TemperatureLevel newTarget = TemperatureHelper.getTemperatureAtPos(player.level(), player.blockPosition());
+            if (!player.hasEffect(TANEffects.CLIMATE_CLEMENCY)) {
+                int changeDelay = ModConfig.temperature.temperatureChangeDelay;
+                TemperatureLevel current = temperature.getTargetLevel();
+                TemperatureLevel newTarget = TemperatureHelper.getTemperatureAtPos(player.level(), player.blockPosition());
 
-            for (var modifier : BuiltInTemperatureModifier.getTemperatureModifierOrder()) {
-                var out = modifier.apply(player, newTarget, changeDelay);
-                newTarget = out.getA();
-                changeDelay = out.getB();
-            }
-
-            if (newTarget != current) {
-                temperature.setTargetLevel(newTarget);
-                if ((temperature.getLevel() == TemperatureLevel.ICY || temperature.getLevel() == TemperatureLevel.HOT)
-                        && newTarget != TemperatureLevel.ICY && newTarget != TemperatureLevel.HOT) {
-                    changeDelay = Math.min(changeDelay, ModConfig.temperature.extremityReboundTemperatureChangeDelay);
+                for (var modifier : BuiltInTemperatureModifier.getTemperatureModifierOrder()) {
+                    var out = modifier.apply(player, newTarget, changeDelay);
+                    newTarget = out.getA();
+                    changeDelay = out.getB();
                 }
-                temperature.setChangeDelayTicks(changeDelay);
-            }
 
-            if (temperature.getChangeDelayTicks() == 0 && current != temperature.getLevel()) {
-                int delta = Integer.compare(temperature.getTargetLevel().ordinal(), temperature.getLevel().ordinal());
-                temperature.setLevel(temperature.getLevel().increment(delta));
-                temperature.setChangeDelayTicks(changeDelay);
-            }
-        } else {
-            temperature.setLevel(TemperatureLevel.NEUTRAL);
-        }
+                if (newTarget != current) {
+                    temperature.setTargetLevel(newTarget);
+                    if ((temperature.getLevel() == TemperatureLevel.ICY || temperature.getLevel() == TemperatureLevel.HOT)
+                            && newTarget != TemperatureLevel.ICY && newTarget != TemperatureLevel.HOT) {
+                        changeDelay = Math.min(changeDelay, ModConfig.temperature.extremityReboundTemperatureChangeDelay);
+                    }
+                    temperature.setChangeDelayTicks(changeDelay);
+                }
 
-        temperature.setExtremityDelayTicks(Math.max(0, temperature.getExtremityDelayTicks() - 1));
-
-        if (temperature.getLastLevel() != temperature.getLevel() && (temperature.getLevel() == TemperatureLevel.ICY || temperature.getLevel() == TemperatureLevel.HOT)) {
-            temperature.setExtremityDelayTicks(ModConfig.temperature.extremityDamageDelay);
-        }
-
-        int hyperthermic = temperature.getHyperthermiaTicks();
-        int maxHyper = TemperatureHelper.getTicksRequiredForHyperthermia();
-
-        if (!player.isCreative() && !player.isSpectator()) {
-            if (!player.hasEffect(MobEffects.FIRE_RESISTANCE) && temperature.getLevel() == TemperatureLevel.HOT && temperature.getExtremityDelayTicks() == 0) {
-                temperature.setHyperthermiaTicks(Math.min(maxHyper, hyperthermic + 1));
+                if (temperature.getChangeDelayTicks() == 0 && current != temperature.getLevel()) {
+                    int delta = Integer.compare(temperature.getTargetLevel().ordinal(), temperature.getLevel().ordinal());
+                    temperature.setLevel(temperature.getLevel().increment(delta));
+                    temperature.setChangeDelayTicks(changeDelay);
+                }
             } else {
-                temperature.setHyperthermiaTicks(Math.max(0, hyperthermic - 2));
+                temperature.setLevel(TemperatureLevel.NEUTRAL);
             }
-        } else {
-            if (temperature.getHyperthermiaTicks() > 0) {
-                temperature.setHyperthermiaTicks(Math.max(0, hyperthermic - 2));
-            }
-        }
-        
-        ResourceLocation SPEED_MODIFIER_ID = ResourceLocation.fromNamespaceAndPath("toughasnails", "speed_modifier");
-        AttributeInstance attribute = player.getAttribute(Attributes.MOVEMENT_SPEED);
-        if (attribute != null && attribute.getModifier(SPEED_MODIFIER_ID) != null) {
-            attribute.removeModifier(SPEED_MODIFIER_ID);
-        }
 
-        if (!player.level().getBlockState(player.getOnPos()).isAir()) {
-            int ticks = TemperatureHelper.getTicksHyperthermic(player);
-            if (ticks > 0) {
-                AttributeInstance attributeinstance = player.getAttribute(Attributes.MOVEMENT_SPEED);
-                if (attributeinstance == null) {return;}
-                float f = 0.015F * TemperatureHelper.getPercentHyperthermic(player);
-                attributeinstance.addTransientModifier(new AttributeModifier(SPEED_MODIFIER_ID, (double)f, Operation.ADD_VALUE));
-            }
-        }
+            temperature.setExtremityDelayTicks(Math.max(0, temperature.getExtremityDelayTicks() - 1));
 
-        if (player.tickCount % 40 == 0 && TemperatureHelper.isFullyHyperthermic(player)) {
-            player.hurt(player.damageSources().source(TANDamageTypes.HYPERTHERMIA), 1);
-        }
+            if (temperature.getLastLevel() != temperature.getLevel() && (temperature.getLevel() == TemperatureLevel.ICY || temperature.getLevel() == TemperatureLevel.HOT)) {
+                temperature.setExtremityDelayTicks(ModConfig.temperature.extremityDamageDelay);
+            }
+
+            int hyperthermic = temperature.getHyperthermiaTicks();
+            int maxHyper = TemperatureHelper.getTicksRequiredForHyperthermia();
+
+            if (!player.isCreative() && !player.isSpectator()) {
+                if (!player.hasEffect(MobEffects.FIRE_RESISTANCE) && temperature.getLevel() == TemperatureLevel.HOT && temperature.getExtremityDelayTicks() == 0) {
+                    temperature.setHyperthermiaTicks(Math.min(maxHyper, hyperthermic + 1));
+                } else {
+                    temperature.setHyperthermiaTicks(Math.max(0, hyperthermic - 2));
+                }
+            } else {
+                if (temperature.getHyperthermiaTicks() > 0) {
+                    temperature.setHyperthermiaTicks(Math.max(0, hyperthermic - 2));
+                }
+            }
+            
+            ResourceLocation SPEED_MODIFIER_ID = ResourceLocation.fromNamespaceAndPath("toughasnails", "speed_modifier");
+            AttributeInstance attribute = player.getAttribute(Attributes.MOVEMENT_SPEED);
+            if (attribute != null && attribute.getModifier(SPEED_MODIFIER_ID) != null) {
+                attribute.removeModifier(SPEED_MODIFIER_ID);
+            }
+
+            if (!player.level().getBlockState(player.getOnPos()).isAir()) {
+                int ticks = TemperatureHelper.getTicksHyperthermic(player);
+                if (ticks > 0) {
+                    AttributeInstance attributeinstance = player.getAttribute(Attributes.MOVEMENT_SPEED);
+                    if (attributeinstance == null) {return;}
+                    float f = 0.015F * TemperatureHelper.getPercentHyperthermic(player);
+                    attributeinstance.addTransientModifier(new AttributeModifier(SPEED_MODIFIER_ID, (double)f, Operation.ADD_VALUE));
+                }
+            }
+
+            if (player.tickCount % 40 == 0 && TemperatureHelper.isFullyHyperthermic(player)) {
+                player.hurt(player.damageSources().source(TANDamageTypes.HYPERTHERMIA), 1);
+            }
+        */
 
         //=========== Sync ===========\\
         ModPackets.HANDLER.sendToPlayer(new UpdateThirstPacket(thirst.getThirst(), thirst.getHydration()), (ServerPlayer)player);
